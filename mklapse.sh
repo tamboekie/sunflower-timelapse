9e2bde886a3a79dffed1cc5b906f20023910f4e9
tamboekie <tamboekie@gmail.com>
2016-08-28 22:57:29 +0100

    Add simple command-line options
    Add -r|--rate to set the video frame rate; -o|--outfile to set the video filename

--- mklapse.sh
+++ mklapse.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
+echo " "
+echo "mklapse, v0.1"
+echo "----------------------------------------------"
 
 # Globals settings
+RATE=60
 input=`pwd`
 output=${input}/crop
-video=timelapse.mp4
+OUTFILE=out_mklapse.mp4
+
+# Use -gt 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use -gt 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to -gt 0 the /etc/hosts part is not recognized ( may be a bug )
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -r|--rate)
+    RATE="$2"
+    shift # past argument
+    ;;
+    -o|--outfile)
+    OUTFILE="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+echo "Frame Rate        = ${RATE}"
+echo "Output video file = ${OUTFILE}"
+echo "----------------------------------------------"
+
 
 mkdir -p ${output}
 
@@ -33,6 +67,6 @@ timelapse-deflicker.pl
 # Finally rename files and make a video
 cd Deflickered
 ls *.jpg| awk 'BEGIN{ a=0 }{ printf "mv %s img%06d.JPG\n", $0, a++ }' | bash
-ffmpeg -y -r 30 -i img%06d.JPG -r 30 -vcodec libx264 -preset slow -crf 18.0 -vf crop=1920:1080,scale=iw:ih,unsharp,hqdn3d ${video}
+ffmpeg -y -r "${RATE}" -i img%06d.JPG -r "${RATE}" -vcodec libx264 -preset slow -crf 18.0 -vf crop=1920:1080,scale=iw:ih,unsharp,hqdn3d "${OUTFILE}"
 
 exit 0
