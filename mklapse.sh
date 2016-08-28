223dca6651e9818e4534ef74fdf9bdd543f51b55
tamboekie <tamboekie@gmail.com>
2016-08-28 18:25:42 +0100

    Encode video at higher quality
    Video is encoded at higher quality to avoid a loss of detail

--- mklapse.sh
+++ mklapse.sh
@@ -33,6 +33,6 @@ timelapse-deflicker.pl
 # Finally rename files and make a video
 cd Deflickered
 ls *.jpg| awk 'BEGIN{ a=0 }{ printf "mv %s img%06d.JPG\n", $0, a++ }' | bash
-avconv -y -r 30 -i img%06d.JPG -r 30 -vcodec libx264 -crf 22.0 -vf crop=1920:1080,scale=iw:ih,unsharp,hqdn3d ${video}
+ffmpeg -y -r 30 -i img%06d.JPG -r 30 -vcodec libx264 -preset slow -crf 18.0 -vf crop=1920:1080,scale=iw:ih,unsharp,hqdn3d ${video}
 
 exit 0
