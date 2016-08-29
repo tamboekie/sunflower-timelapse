02cba34943a1eb3491eb1947b854554329295363
tamboekie <tamboekie@gmail.com>
2016-08-29 21:57:18 +0100

    Non-functional clean-up

--- sunflowercam.sh
+++ sunflowercam.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
 DATE=$(date +"%Y%m%d_%H%M%S")
 
 raspistill -q 97 -vf -hf -ex night -a 12 -o sun$DATE.jpg -v
-#raspistill -q 97 -vf -hf -ex night -a 12 -o matrix_drc/A${DATE}_cloud.jpg -v --awb cloud --metering matrix --drc med
-#raspistill -q 97 -vf -hf -ex night -a 12 -o matrix_drc/A${DATE}_shade.jpg -v --awb shade --metering matrix --drc med
-#raspistill -q 97 -vf -hf -ex night -a 12 -o matrix_drc/A${DATE}_flo.jpg -v --awb fluorescent --metering matrix --drc med
-#raspistill -q 97 -vf -hf -ex night -a 12 -o drc_low/sun${DATE}.jpg -v --drc low
-#raspistill -q 97 -vf -hf -ex night -a 12 -o drc_med/sun${DATE}.jpg -v --drc med
-
-
